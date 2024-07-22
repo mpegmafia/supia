96cb0b26f6c069a88b3dda49597b1ce10d5bd7e3
jihyeon-yu <fbwlgus6@naver.com>
2024-07-22 23:59:59 +0900

    Add: add google login

--- supia-back/src/main/java/com/forest/supia/security/SecurityConfig.java
+++ supia-back/src/main/java/com/forest/supia/security/SecurityConfig.java
@@ -1,10 +1,39 @@
 package com.forest.supia.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/", "/login", "/error").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2Login ->
+                        oauth2Login
+                                .loginPage("/login")
+                                .userInfoEndpoint(userInfoEndpoint ->
+                                        userInfoEndpoint.oidcUserService(oidcUserService())
+                                )
+                                .successHandler(new SimpleUrlAuthenticationSuccessHandler("/"))
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public OidcUserService oidcUserService() {
+        return new OidcUserService();
+    }
 }
